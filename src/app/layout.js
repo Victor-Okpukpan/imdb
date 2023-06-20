1417c7746d88ebbf64a2d200834e9a0681cbd8fd
Victor-Okpukpan <vokpukpan@gmail.com>
2023-06-20 14:59:41 +0100

    Added Dark mode functionality

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Providers from "./Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,12 +14,14 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* Header */}
-        <Header />
-        {/* Navbar */}
+        <Providers>
+          {/* Header */}
+          <Header />
+          {/* Navbar */}
 
-        {/* SearchBox */}
-        {children}
+          {/* SearchBox */}
+          {children}
+        </Providers>
       </body>
     </html>
   );
